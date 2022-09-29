51cf7ef6a2758884e271af73fdb687ae45a993a2
jujuwon <webmon@kakao.com>
2022-09-29 20:03:51 +0900

    [AM-270] feat: 앱 버전 조회 API 구현 (#33)

---
+++ src/main/java/com/amondfarm/api/domain/Version.java
@@ -0,0 +1,39 @@
+package com.amondfarm.api.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.amondfarm.api.domain.enums.version.VersionStatus;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "app_version")
+public class Version {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false)
+	private String version;
+
+	@Column(nullable = false, columnDefinition = "TINYINT", length = 1)
+	private int isRequired;
+
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private VersionStatus status;
+
+	private String releaseNote;
+}
