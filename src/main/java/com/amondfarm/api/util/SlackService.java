29723325b84165e8bb5da65133c787ad1b99e60e
jujuwon <webmon@kakao.com>
2022-10-03 17:33:12 +0900

    [AM-274] feat: Noti 전송 상태 변경 추가 및 Firebase SDK 파일 Action 추가 (#37)

--- src/main/java/com/amondfarm/api/util/SlackService.java
+++ src/main/java/com/amondfarm/api/util/SlackService.java
@@ -197,6 +197,7 @@ public class SlackService {
 		if (deviceToken != null) {
 			try {
 				fcmService.sendMessageTo(deviceToken, "미션 인증 완료", "수행하신 미션이 인증되었어요. 보상을 받아가세요!");
+				userMission.sendNotification();
 			} catch (IOException e) {
 				log.error("FCM 메시지를 보내는 데에 실패했습니다.");
 			}
@@ -215,6 +216,7 @@ public class SlackService {
 		if (deviceToken != null) {
 			try {
 				fcmService.sendMessageTo(deviceToken, "미션 인증 반려", "수행하신 미션이 반려 처리되었어요.");
+				userMission.sendNotification();
 			} catch (IOException e) {
 				log.error("FCM 메시지를 보내는 데에 실패했습니다.");
 			}
