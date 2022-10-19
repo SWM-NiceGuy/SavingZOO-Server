d3230d31803c08d73a624d309d3be96ac09c8493
jujuwon <webmon@kakao.com>
2022-10-19 16:18:03 +0900

    fix: 개발 서버 Batch 종료

--- src/main/java/com/amondfarm/api/config/JobConfig.java
+++ src/main/java/com/amondfarm/api/config/JobConfig.java
@@ -27,29 +27,29 @@ public class JobConfig {
 
 	private final Scheduler scheduler;
 
-	@PostConstruct
-	public void start() {
-		JobDetail insetMissionJobDetail = buildJobDetail(DailyMissionJob.class, new HashMap());
-		JobDetail pushJobDetail = buildJobDetail(DailyPushJob.class, new HashMap());
-
-		try {
-			scheduler.scheduleJob(insetMissionJobDetail, buildJobTrigger("0 0 0 * * ?"));
-			scheduler.scheduleJob(pushJobDetail, buildJobTrigger("0 0 9 * * ?"));
-
-		} catch (SchedulerException e) {
-			e.printStackTrace();
-		}
-	}
-
-	public Trigger buildJobTrigger(String scheduleExp) {
-		return TriggerBuilder.newTrigger()
-			.withSchedule(CronScheduleBuilder.cronSchedule(scheduleExp)).build();
-	}
-
-	public JobDetail buildJobDetail(Class job, Map params) {
-		JobDataMap jobDataMap = new JobDataMap();
-		jobDataMap.putAll(params);
-
-		return newJob(job).usingJobData(jobDataMap).build();
-	}
+	// @PostConstruct
+	// public void start() {
+	// 	JobDetail insetMissionJobDetail = buildJobDetail(DailyMissionJob.class, new HashMap());
+	// 	JobDetail pushJobDetail = buildJobDetail(DailyPushJob.class, new HashMap());
+	//
+	// 	try {
+	// 		scheduler.scheduleJob(insetMissionJobDetail, buildJobTrigger("0 0 0 * * ?"));
+	// 		scheduler.scheduleJob(pushJobDetail, buildJobTrigger("0 0 9 * * ?"));
+	//
+	// 	} catch (SchedulerException e) {
+	// 		e.printStackTrace();
+	// 	}
+	// }
+	//
+	// public Trigger buildJobTrigger(String scheduleExp) {
+	// 	return TriggerBuilder.newTrigger()
+	// 		.withSchedule(CronScheduleBuilder.cronSchedule(scheduleExp)).build();
+	// }
+	//
+	// public JobDetail buildJobDetail(Class job, Map params) {
+	// 	JobDataMap jobDataMap = new JobDataMap();
+	// 	jobDataMap.putAll(params);
+	//
+	// 	return newJob(job).usingJobData(jobDataMap).build();
+	// }
 }
